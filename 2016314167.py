66143d0a9e5c7750c9982bcffbfec4d14cd5c13c
Heesoo Yang <didgmltn7769@naver.com>
2021-04-02 21:41:43 +0900

    [edit] total

--- 2016314167.py
+++ 2016314167.py
@@ -3,45 +3,55 @@ import os
 import threading
 
 
-def socket_programming():
+def socket_programming(connectionSocket):
 
   while True:
     msg = connectionSocket.recv(1024).decode()
     print(msg)
-    filename = msg.split()[1].split('/')[1]
-    print(filename)
-    httpResHeader=''
     if not msg:
       print('bye')
-      lock.release()
+      connectionSocket.close()
       break
+
+    filename = msg.split()[1].split('/')[1]
+    httpResHeader=''
 
     if(os.path.isfile(filename)):
       filetype = filename.split('.')[1]
       if(filetype=='html'):
-        file = open(filename, 'rt', encoding='utf8')
+        file = open(filename, 'rb')
         sendData = file.read()
+        # print(sendData)
         file.close()
         httpResHeader += 'HTTP/1.1 200 OK\r\n'
+        httpResHeader += 'keep-alive: 120\r\n'
         httpResHeader += 'connection: keep-alive\r\n'
         httpResHeader += 'content-type: text/html; charset=utf-8\r\n'
         httpResHeader += '\r\n'
-        httpResHeader +=sendData
-        connectionSocket.send(httpResHeader.encode())
+        data = httpResHeader.encode()
+        data += sendData
+        connectionSocket.send(data)
         #connectionSocket.close()
       else:
+        print(filetype)
+        print(filename)
         file = open(filename, 'rb')
         sendData = file.read()
         file.close()
         httpResHeader += 'HTTP/1.1 200 OK\r\n'
+        httpResHeader += 'keep-alive: 120\r\n'
         httpResHeader += 'connection: keep-alive\r\n'
-        httpResHeader += 'content-type: text/html; charset=utf-8\r\n'
+        httpResHeader += 'content-type: image/png; charset=utf-8\r\n'
+        # httpResHeader += 'content-length: %s\r\n' %len(sendData)
         httpResHeader += '\r\n'
-        httpResHeader += sendData
-        connectionSocket.send(httpResHeader.encode())
+        data = httpResHeader.encode()
+        data += sendData
+        connectionSocket.send(data)
+        # connectionSocket.send()
         # connectionSocket.close()
     else:
       httpResHeader += 'HTTP/1.1 404 Not Found\r\n'
+      httpResHeader += 'keep-alive: 120\r\n'
       httpResHeader += 'connection: keep-alive\r\n'
       httpResHeader += 'content-type: text/html; charset=utf-8\r\n'
       httpResHeader += '\r\n'
@@ -52,7 +62,7 @@ def socket_programming():
 
 
 if __name__ == "__main__":
-  lock = threading.Lock()
+  # lock = threading.Lock()
   serverPort = 10080
   serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   serverSocket.bind(('', serverPort))
@@ -60,6 +70,5 @@ if __name__ == "__main__":
   while True:
     connectionSocket, addr = serverSocket.accept()
     # lock.acquire()
-    t = threading.Thread(target=socket_programming)
+    t=threading.Thread(target=socket_programming, args=(connectionSocket,))
     t.start()
-  connectionSocket.close()
