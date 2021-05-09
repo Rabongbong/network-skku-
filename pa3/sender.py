627531f40a9a4037a08a0ff726ad9c3c06454d18
yhsooi <didgmltn7769@naver.com>
2021-05-09 10:45:44 +0900

    [edit] header

--- pa3/sender.py
+++ pa3/sender.py
@@ -34,7 +34,7 @@ def fileSender(recvAddr, windowSize, srcFilename, dstFilename):
   checkACK={}      # check ACK is arrived or not
   timeBuffer={}    # store time for drop
   flag=0           # 마지막 패킷인지 확인 (마지막 패킷:1)
-  ack=0            # ack #
+  ack=-1           # ack #
   duplicateFlag =0 # check duplicate
   timeOut = 1.0
   sendWindowSize=windowSize
@@ -48,10 +48,10 @@ def fileSender(recvAddr, windowSize, srcFilename, dstFilename):
 
   # 초기에 packet 보내기(header size:100(filename:49 + serialnumber:50 + flag:1) + body size:1300)
   while sendWindowSize >0:
-    serialNumber = ack + windowSize-sendWindowSize
+    serialNumber = ack + 1 + windowSize-sendWindowSize
     if serialNumber > lastSerialNumber:
       break
-
+    resHeader=''
     resHeader += dstFilename
     resHeader += '\0' * (49-len(dstFilename))
     resHeader += '0' * (50-len(str(serialNumber)))
@@ -65,16 +65,15 @@ def fileSender(recvAddr, windowSize, srcFilename, dstFilename):
     timeBuffer[serialNumber]=time.time()  # store time
     logProc.writePkt(serialNumber, 'sent')
     windowSize -= 1
-    serialNumber += 1
 
   while True:
     try:
-      newMsg, recvAddr = senderSocket.recvfrom(1400)
+      newMsg, recvAddr = senderSocket.recvfrom(150)
 
     except timeout:
 
-      outTime = round(timeBuffer[ack]-start_time, 3)
-      senderSocket.sendto(sendBuffer[ack], (recvAddr, serverPort))
+      outTime = round(timeBuffer[ack+1]-start_time, 3)
+      senderSocket.sendto(sendBuffer[ack+1], (recvAddr, serverPort))
 
       logProc.writePkt(ack, 'timeout since ' + str(outTime) + '(time out value ' + str(round(timeOut, 3)) + ')')
       logProc.writePkt(ack, 'retransmitted')
@@ -82,7 +81,7 @@ def fileSender(recvAddr, windowSize, srcFilename, dstFilename):
 
 
     else:
-      ACK = newMsg.decode()
+      ACK = int(newMsg.decode())
       logProc.writeAck(ACK, 'received')
 
       try:
@@ -96,8 +95,8 @@ def fileSender(recvAddr, windowSize, srcFilename, dstFilename):
       elif ACK == ack:
         if duplicateFlag==3:
           senderSocket.sendto(sendBuffer[ACK+1], (recvAddr, serverPort))
-          logProc.writePkt(serialNumber, '3 duplicated ACKs')
-          logProc.writePkt(serialNumber+1, 'retransmitted')
+          logProc.writePkt(ack, '3 duplicated ACKs')
+          logProc.writePkt(ack+1, 'retransmitted')
           duplicateFlag=0
 
       elif ACK > ack:
@@ -118,9 +117,10 @@ def fileSender(recvAddr, windowSize, srcFilename, dstFilename):
           serialNumber = ack + 1 + windowSize-sendWindowSize
           if serialNumber > lastSerialNumber:
             break
+
           if serialNumber ==lastSerialNumber:
             flag=1
-
+          resHeader=''
           resHeader += dstFilename
           resHeader += '\0' * (49-len(dstFilename))
           resHeader += '0' * (50-len(str(serialNumber)))
