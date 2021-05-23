89ac09bec34d486c7758b4a4a062bbcdef6f69c3
mininet <didgmltn7769@naver.com>
2021-05-23 19:03:22 +0900

    [edit] nat

--- pa4/client.py
+++ pa4/client.py
@@ -1,24 +1,87 @@
 import sys
 import socket
+import threading
 
 serverIP = '10.0.0.3'
 serverPort = 10080
 clientPort = 10081
+
+# save allClient's information
+allClient={}
+
+#Client Socket
+clientSocket = None
+
+#destination of server(ip, port)
+
+def socket_programming():
+    global clientSocket
+    global allClient
+
+    while True:
+        packet, senderAddress = clientSocket.recvfrom(1000)
+        flag = packet.decode().split(":")[0]
+        if flag == "0":    # register client
+            name = packet.decode().split(":")[1]
+            ip = packet.decode().split(":")[2]
+            port = int(packet.decode().split(":")[3])
+            allClient[name]=(ip, port)
+        elif flag == "1":  # deregister client
+            name = packet.decode().split(":")[1]
+            del allClient[name]
+        elif flag == "2":
+            name = packet.decode().split(":")[1]
+            msg = packet.decode().split(":")[2]
+            print("From " + name + " [" + msg + " ]")
+
+
+def showList():
+    global allClient
+    for key, value in allClient.items():
+        print(key + "\t\t" + value[0]+":"+ str(value[1]))
+
+
+def exitChat(clientID):
+    dest = (serverIP, serverPort)
+    clientSocket.sendto(("1:"+clientID).encode(), dest)
+    exit()
+
+
+def chat(clientID, text):
+    global clientSocket
+    global allClient
+    Name = text.split(" ")[1]
+    msg = text[6+len(Name):]
+    print(msg)
+    print(Name)
+    clientSocket.sendto(("2:"+clientID + ":" +msg).encode(), allClient[Name])
 
 
 def client(serverIP, serverPort, clientID):
     """
     Write your code!!!
     """
-    allClient={}
-    dest = (serverIP, serverPort)
-    print(dest)
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    clientSocket.bind(('',clientPort))
-    clientSocket.sendto(clientID.encode(), dest)
+    global allClient
+    global clientSocket
 
-    packet, senderAddress = clientSocket.recvfrom(100)
-    print(packet.decode())
+
+    dest = (serverIP, serverPort)
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    clientSocket.sendto(("0:"+clientID).encode(), dest)
+
+    t=threading.Thread(target=socket_programming)
+    t.start()
+
+    while True:
+        method = input("")
+
+        if method == "@show_list":
+            showList()
+        elif method == "@exit":
+            exitChat(clientID)
+        else:
+            chat(clientID, method)
+
     pass
 
 
--- pa4/server.py
+++ pa4/server.py
@@ -11,13 +11,38 @@ def server():
     allClient={}
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     serverSocket.bind(('', serverPort))
-    print(1)
-    packet, senderAddress = serverSocket.recvfrom(1000)
-    print(2)
-    print(senderAddress)
-    print(3)
-    serverSocket.sendto(packet, senderAddress)
-    print(4)
+    
+    while True:
+        packet, senderAddress = serverSocket.recvfrom(1000)
+        flag = packet.decode().split(":")[0]
+
+        if flag == "0":   #register new client
+            clientInfo = packet.decode().split(":")[1]
+            allClient[clientInfo]=senderAddress
+            ip = senderAddress[0]
+            port = str(senderAddress[1])
+            info = ip +":" + port
+            print(clientInfo + "\t\t" + info)
+
+            for key, value in allClient.items():
+                data = ("0:" + clientInfo + ":").encode()
+                data+=info.encode()
+                serverSocket.sendto(data, value)
+            for key, value in allClient.items():
+                data = ("0:" + key + ":").encode()
+                data+= (value[0]+":").encode() + str(value[1]).encode()
+                serverSocket.sendto(data, senderAddress)
+
+        elif flag == "1":    # deregister client
+            clientInfo = packet.decode().split(":")[1]
+            ip = senderAddress[0]
+            port = str(senderAddress[1])
+            info = ip +":" + port
+            print(clientInfo + "  is deregistered\t\t" + info)
+            del allClient[clientInfo]
+            for key, value in allClient.items():
+                data = ("1:" + clientInfo).encode()
+                serverSocket.sendto(data, value)
     pass
 
 
