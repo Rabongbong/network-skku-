d8c2c1c7f4fe617380cdb661ffd38e351f3a5979
mininet <didgmltn7769@naver.com>
2021-05-25 16:33:18 +0900

    [edit] server

--- pa4/client.py
+++ pa4/client.py
@@ -1,49 +1,75 @@
 import sys
 import socket
 import threading
+import time
 
 serverIP = '10.0.0.3'
 serverPort = 10080
 clientPort = 10081
 
-# save allClient's information
+# save allClient's information(NAT)
 allClient={}
+
+# save allClient's information(local)
+allLocalClient={}
 
 #Client Socket
 clientSocket = None
 
-#destination of server(ip, port)
+# Eflag for terminate thread
+Eflag=0
 
 def socket_programming():
     global clientSocket
     global allClient
+    global allLocalClient
+    global Eflag
+
+    clientSocket.settimeout(3)
 
     while True:
-        packet, senderAddress = clientSocket.recvfrom(1000)
-        flag = packet.decode().split(":")[0]
-        if flag == "0":    # register client
-            name = packet.decode().split(":")[1]
-            ip = packet.decode().split(":")[2]
-            port = int(packet.decode().split(":")[3])
-            allClient[name]=(ip, port)
-        elif flag == "1":  # deregister client
-            name = packet.decode().split(":")[1]
-            del allClient[name]
-        elif flag == "2":
-            name = packet.decode().split(":")[1]
-            msg = packet.decode().split(":")[2]
-            print("From " + name + " [" + msg + " ]")
+        try:
+            packet, senderAddress = clientSocket.recvfrom(300)
+
+        except socket.timeout:
+            pass
+
+        else:
+            flag = packet.decode().split(":")[0]
+            if flag == "0":    # register client
+                name = packet.decode().split(":")[1]
+                ip = packet.decode().split(":")[2]
+                port = int(packet.decode().split(":")[3])
+                localIp = packet.decode().split(":")[4]
+                localPort = int(packet.decode().split(":")[5])
+                allClient[name]=(ip, port)
+                allLocalClient[name]= (localIp, localPort)
+            elif flag == "1":  # deregister client
+                name = packet.decode().split(":")[1]
+                del allClient[name]
+            elif flag == "2":
+                name = packet.decode().split(":")[1]
+                msg = packet.decode().split(":")[2]
+                print("From " + name + " [" + msg + " ]")
+        
+        finally:
+            if Eflag == 1:
+                break
 
 
 def showList():
     global allClient
+    global allLocalClient
     for key, value in allClient.items():
         print(key + "\t\t" + value[0]+":"+ str(value[1]))
 
-
 def exitChat(clientID):
+
+    global Eflag
+
     dest = (serverIP, serverPort)
     clientSocket.sendto(("1:"+clientID).encode(), dest)
+    Eflag=1
     exit()
 
 
@@ -51,10 +77,27 @@ def chat(clientID, text):
     global clientSocket
     global allClient
     Name = text.split(" ")[1]
+    clientIp = allClient[Name][0]
+    myIp = allClient[clientID][0]
     msg = text[6+len(Name):]
-    print(msg)
-    print(Name)
-    clientSocket.sendto(("2:"+clientID + ":" +msg).encode(), allClient[Name])
+    if clientIp==myIp:
+        clientSocket.sendto(("2:"+clientID + ":" +msg).encode(), allLocalClient[Name])
+    else:
+        clientSocket.sendto(("2:"+clientID + ":" +msg).encode(), allClient[Name])
+
+# send for keep alive
+def keep_connect(clientID):
+
+    global clientSocket
+    global Eflag
+    dest = (serverIP, serverPort)
+    while True:
+        time.sleep(10)
+        if Eflag ==1:
+            break
+        clientSocket.sendto(("3:"+clientID).encode(), dest)
+        if Eflag ==1:
+            break
 
 
 def client(serverIP, serverPort, clientID):
@@ -63,14 +106,23 @@ def client(serverIP, serverPort, clientID):
     """
     global allClient
     global clientSocket
+    global clientPort
 
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", clientPort))
+    ip_address = s.getsockname()[0]
+    s.close()
 
     dest = (serverIP, serverPort)
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    clientSocket.sendto(("0:"+clientID).encode(), dest)
+    clientSocket.bind(('', clientPort))
+    clientSocket.sendto(("0:"+clientID + ":" + ip_address + ":" + str(clientPort)).encode(), dest)
 
     t=threading.Thread(target=socket_programming)
     t.start()
+
+    t1=threading.Thread(target=keep_connect, args=(clientID,))
+    t1.start()
 
     while True:
         method = input("")
@@ -82,14 +134,9 @@ def client(serverIP, serverPort, clientID):
         else:
             chat(clientID, method)
 
-    pass
-
-
 """
 Don't touch the code below!
 """
 if  __name__ == '__main__':
     clientID = input("")
     client(serverIP, serverPort, clientID)
-
-
--- pa4/server.py
+++ pa4/server.py
@@ -1,55 +1,151 @@
 import sys
-from time import sleep
+import time
 import socket
+import threading
 
 serverPort = 10080
+# save allClient's information
+allClient={}
+
+# save time for all clients
+connectTime={}
+
+#server Socket
+serverSocket = None
+
+#lock
+lock = None
+
+
+def address(value):
+    ip = value.split(":")[0]
+    port = value.split(":")[1]
+
+    return (ip, int(port))
+
+
+def check_alive():
+
+    global serverSocket
+    global connectTime
+    global lock
+    item =""
+    lock.acquire()
+    for key, value in connectTime.items():
+        if (time.time() - value) > 30:
+            disappear(key)
+            item=key
+    lock.release()
+
+    if item!="":
+        del connectTime[item]
+
+
+def disappear(client):
+    
+    global allClient
+    global serverSocket
+    global connectTime
+    global lock
+
+    ip = allClient[client].split(":")[0]
+    port = allClient[client].split(":")[1]
+    info = ip +":" + str(port)
+    print(client + " is disappeared \t\t" + info)
+    del allClient[client]
+    for key, value in allClient.items():
+        data = ("1:" + client).encode()
+        serverSocket.sendto(data, address(value))
+
+
+def register_client(packet, senderAddress):
+
+    global allClient
+    global connectTime
+    global serverSocket
+    global lock
+
+    clientInfo = packet.decode().split(":")[1]
+    localIp = packet.decode().split(":")[2]
+    localPort = packet.decode().split(":")[3]
+    connectTime[clientInfo]=time.time()
+    ip = senderAddress[0]
+    port = str(senderAddress[1])
+    info = ip +":" + port
+    print(clientInfo + "\t\t" + info)
+    allClient[clientInfo]= info + ":" + localIp + ":" + localPort
+
+    for key, value in allClient.items():
+        data = ("0:" + clientInfo + ":").encode()
+        data+= allClient[clientInfo].encode()
+        serverSocket.sendto(data, address(value))
+    for key, value in allClient.items():
+        data = ("0:" + key + ":").encode()
+        data+= value.encode()
+        serverSocket.sendto(data, senderAddress)
+
+
+def unregister_client(packet, senderAddress):
+
+    global allClient
+    global serverSocket
+    global connectTime
+    global lock
+
+    clientInfo = packet.decode().split(":")[1]
+    ip = senderAddress[0]
+    port = str(senderAddress[1])
+    info = ip +":" + port
+    print(clientInfo + "  is deregistered\t\t" + info)
+    del allClient[clientInfo]
+    lock.acquire()
+    del connectTime[clientInfo]
+    lock.release()
+    for key, value in allClient.items():
+        data = ("1:" + clientInfo).encode()
+        serverSocket.sendto(data, address(value))
+
+
+def keep_alive(packet):
+
+    global connectTime
+    global lock
+
+    clientInfo = packet.decode().split(":")[1]
+    connectTime[clientInfo]=time.time()
+
 
 def server():
     """
     Write your code!!!
     """
-    allClient={}
+    global allClient
+    global connectTime
+    global serverSocket
+    global lock
+
+    lock = threading.Lock()
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     serverSocket.bind(('', serverPort))
-    
+
     while True:
-        packet, senderAddress = serverSocket.recvfrom(1000)
+        packet, senderAddress = serverSocket.recvfrom(100)
         flag = packet.decode().split(":")[0]
+        print(packet.decode())
 
-        if flag == "0":   #register new client
-            clientInfo = packet.decode().split(":")[1]
-            allClient[clientInfo]=senderAddress
-            ip = senderAddress[0]
-            port = str(senderAddress[1])
-            info = ip +":" + port
-            print(clientInfo + "\t\t" + info)
-
-            for key, value in allClient.items():
-                data = ("0:" + clientInfo + ":").encode()
-                data+=info.encode()
-                serverSocket.sendto(data, value)
-            for key, value in allClient.items():
-                data = ("0:" + key + ":").encode()
-                data+= (value[0]+":").encode() + str(value[1]).encode()
-                serverSocket.sendto(data, senderAddress)
+        if flag == "0":      # register new client
+           register_client(packet, senderAddress)
 
         elif flag == "1":    # deregister client
-            clientInfo = packet.decode().split(":")[1]
-            ip = senderAddress[0]
-            port = str(senderAddress[1])
-            info = ip +":" + port
-            print(clientInfo + "  is deregistered\t\t" + info)
-            del allClient[clientInfo]
-            for key, value in allClient.items():
-                data = ("1:" + clientInfo).encode()
-                serverSocket.sendto(data, value)
-    pass
+            unregister_client(packet, senderAddress)
 
+        elif flag == "3":    # keep_alive
+            keep_alive(packet)
+
+        check_alive()
 
 """
 Don't touch the code below
 """
 if  __name__ == '__main__':
     server()
-
-
